e9bea87584c56c6afe2365abdef4942e304374b4
Vainottu <94010466+Vainottu@users.noreply.github.com>
2023-03-08 10:32:18 +0200

    Delete jamk/Lab04 directory

--- jamk/Lab04/L04T01.py
+++
@@ -1,8 +0,0 @@
-#Tee ohjelma, joka kysyy käyttäjältä kuinka monta kokonaislukua luodaan.
-#Lisää listaan luvut niin että alkion arvo on alkion indeksi kertaa 10. Huomaa, että indeksit alkavat nollasta (0). Tulosta taulukon sisältö konsoliin.
-
-
-
-n = int(input("Kuinka monta kokonaislukua haluat luoda? "))
-lista = [i*10 for i in range(n)]
-print(lista)
