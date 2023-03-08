62311fc1af9a345cf71671c19a7e288b121dabe6
Vainottu <94010466+Vainottu@users.noreply.github.com>
2023-03-08 10:32:35 +0200

    Add files via upload

--- jamk/TTC2030/Lab04/L04T04.py
+++ jamk/TTC2030/Lab04/L04T04.py
@@ -1,1 +1,7 @@
 #Tee ohjelma, joka kysyy ensin käyttäjältä jonkin luvun väliltä 1-10. Tämän jälkeen näytä luvut yhdestä annettuun lukuun ja luvun neliön.
+
+
+n = int(input("Anna luku väliltä 1-10: "))
+
+for i in range(1, n+1):
+    print(f"{i} {i*i}")
